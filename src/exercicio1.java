9fc0bb3e88b2499da0b404f6d221ccd885e3bdae
Matheus Luiz Bonatto <matheuslbonatto@gmail.com>
2024-04-10 19:35:04 -0300

    ExerciciosResposta

---
+++ src/exercicio1.java
@@ -0,0 +1,9 @@
+public class exercicio1 {
+    public static void main(String[] args) {
+        
+        int age = 25;
+        
+        System.out.printf("I am %s years old!" , age);
+    }
+    
+}
