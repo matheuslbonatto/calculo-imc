73b97d34ae5c4ccc921bd1db1daeff602f4b0791
Matheus Luiz Bonatto <matheuslbonatto@gmail.com>
2024-04-10 19:35:37 -0300

    ExerciciosResultado

---
+++ src/exercicio4.java
@@ -0,0 +1,12 @@
+public class exercicio4 {
+    public static void main(String[] args) {
+        int largura = 12;
+        int altura = 7;
+        double area = largura * altura;
+        double perimetro = 2 * (altura + largura);
+        System.out.printf("A área do retangulo com largura %d e altura %d é: %f",
+            largura, altura, area);
+        System.out.printf("\nO Perimetro do retangulo com largura %d e altura %d é: %f",
+            largura, altura, perimetro);
+      }
+}
